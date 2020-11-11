f40935b1e267e54dc3c3717203440beea7211278
epw7856 <epw7856@gmail.com>
2020-11-10 19:59:45 -0500

    Refactored the DataItem class to be more lightweight.

--- TCP_Client/DataItem.h
+++ TCP_Client/DataItem.h
@@ -1,7 +1,6 @@
 #ifndef DATAITEM_H
 #define DATAITEM_H
 
-#include "DataType.h"
 #include <QString>
 
 class DataItem
@@ -9,41 +8,79 @@ class DataItem
 public:
     DataItem
     (
-        DataType itemType,
+        const QString& itemType,
         const QString& itemName,
         const QString& itemUnits,
-        const QString& itemFormat
+        const QString& itemFormat,
+        const std::pair<QString, QString>& itemRange
     )
     :
         type(itemType),
         name(itemName),
         units(itemUnits),
-        format(itemFormat) {}
+        format(itemFormat),
+        range(itemRange),
+        rangeCheckEnable(!range.first.isEmpty() && !range.second.isEmpty()) {}
 
+    QString getDataItemType() const;
+    QString getDataItemName() const;
     unsigned getRawValue() const;
-    QString getDisplayValue() const;
+    QString getDataItemUnits() const;
+    QString getDataItemFormat() const;
+    std::pair<QString, QString> getDataItemRange() const;
+    bool isRangeCheckingEnabled() const;
 
-    void setRawValue(unsigned value);
-    bool setDisplayValue(const QString& value);
+    void setRawValue(unsigned updatedValue);
 
 private:
-    DataType type = DataType::UnsignedInteger;
+    QString type = "";
     QString name = "";
     unsigned rawValue = 0U;
-    QString displayValue = "???";
     QString units = "";
     QString format = "";
-    bool performRangeChecking = false;
+    std::pair<QString, QString> range = {};
+    bool rangeCheckEnable = false;
+
 };
+
+inline QString DataItem::getDataItemType() const
+{
+    return type;
+}
+
+inline QString DataItem::getDataItemName() const
+{
+    return name;
+}
 
 inline unsigned DataItem::getRawValue() const
 {
     return rawValue;
 }
 
-inline QString DataItem::getDisplayValue() const
+inline QString DataItem::getDataItemUnits() const
 {
-    return displayValue;
+    return units;
+}
+
+inline QString DataItem::getDataItemFormat() const
+{
+    return type;
+}
+
+inline std::pair<QString, QString> DataItem::getDataItemRange() const
+{
+    return range;
+}
+
+inline bool DataItem::isRangeCheckingEnabled() const
+{
+    return rangeCheckEnable;
+}
+
+inline void DataItem::setRawValue(unsigned updatedValue)
+{
+    rawValue = updatedValue;
 }
 
 #endif // DATAITEM_H
